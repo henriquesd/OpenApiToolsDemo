1feff67e677e289fb50d56b0c8b96f364efbcb20
henriquesd <hsd.foz@gmail.com>
2025-04-16 21:03:11 +0200

    Add Swagger, Scalar and ReDoc

--- OpenApiToolsDemo/Program.cs
+++ OpenApiToolsDemo/Program.cs
@@ -1,10 +1,16 @@
+using OpenApiToolsDemo.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddOpenApiConfiguration(builder.Environment);
+
 var app = builder.Build();
+
+app.UseOpenApiConfiguration();
 
 // Configure the HTTP request pipeline.
 
